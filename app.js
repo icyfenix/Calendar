5b7c96aa8c0f29b27f2f9803f7fa9bf4d0ed1f8c
Lillian Chan <californium98@gmail.com>
2013-02-18 21:42:51 -0800

    change app.get to app.post for adding new event

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/calendar/getItems/:num', calendar.getNItems);
-app.get('/calendar/new_event', calendar.newEvent);
+app.post('/calendar/new_event', calendar.newEvent);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
